ab19dedeb4404944894a4fa60c05db951ca0560b
Shekhar Dangi <arpitt682@gmail.com>
2025-09-22 23:43:53 +0530

    feat(web): Add dashboard page and update layout

---
+++ apps/web/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import NavBar from "../../components/NavBar";
+
+export default async function Home() {
+  return <>dashboard</>;
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,3 @@
-import prisma from "@lifeorb/prisma";
-
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  return <>{JSON.stringify(users)}</>;
+  return <>home</>;
 }
